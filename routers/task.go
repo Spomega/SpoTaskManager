1101f1019bdf1480958d92b0118871ff856e77ec
Spomega <spomegasys@gmail.com>
2019-04-04 12:51:03 +0000

    changes to tasks route

--- routers/task.go
+++ routers/task.go
@@ -11,11 +11,7 @@ import (
 func SetTaskRoutes(router *mux.Router) *mux.Router {
 	taskRouter := mux.NewRouter()
 	taskRouter.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")
-	//taskRouter.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT")
 	taskRouter.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")
-	//taskRouter.HandleFunc("/tasks/{id}", controllers.GetTaskByID).Methods("GET")
-	//taskRouter.HandleFunc("/tasks/users/{id}", controller.GetTaskByUser).Methods("GET")
-	//taskRouter.HandleFunc("/tasks/{id}", controller.DeleteTask).Methods("DELETE")
 	router.PathPrefix("/tasks").Handler(negroni.New(
 		negroni.HandlerFunc(common.Authorize),
 		negroni.Wrap(taskRouter),
